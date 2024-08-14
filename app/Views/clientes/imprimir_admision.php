26d5d3be919a869a46be40f865daffb29d3bcf02
Cesar Gomez <00.13.zkr@gmail.com>
2024-08-13 21:01:57 -0600

    actualizacion title

--- app/Views/clientes/imprimir_admision.php
+++ app/Views/clientes/imprimir_admision.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Imprimir Información de Admisión</title>
+    <title>Intake_<?= $cliente['clientID'] ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
