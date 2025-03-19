a7b57567ea6b0144295449a2f8df0793360e95ea
Cesar Gomez <00.13.zkr@gmail.com>
2025-03-19 12:21:25 -0600

    Update intake.php

--- app/Views/intake.php
+++ app/Views/intake.php
@@ -853,6 +853,7 @@
         const Cliente = <?= json_encode($cliente) ?>;
         const Sucursal = <?= json_encode($sucursal) ?>;
     </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.7.8/handlebars.min.js" integrity="sha512-E1dSFxg+wsfJ4HKjutk/WaCzK7S2wv1POn1RRPGh8ZK+ag9l244Vqxji3r6wgz9YBf6+vhQEYJZpSjqWFPg9gg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js" integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS" crossorigin="anonymous"></script>
@@ -862,7 +863,7 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script src="<?= base_url('js/app.js?v=1.0.1') ?>"></script>
-    <script src="<?= base_url("js/intake.js?v=1.0.1") ?>"></script>
+    <script src="<?= base_url("js/intake.js?v=1.0.3") ?>"></script>
 
 </body>
 
